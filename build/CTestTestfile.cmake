ef9d6d6fecd358d9ec5932343f05d3842a0d3b52
Maxim Timoshkin <maximatimoshkin@outlook.com>
2024-04-05 23:44:49 +0300

    build folder removed

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ip111/Code/ListLib
-# Build directory: /home/ip111/Code/ListLib/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/ip111/Code/ListLib/build/MainTest[1]_include.cmake")
